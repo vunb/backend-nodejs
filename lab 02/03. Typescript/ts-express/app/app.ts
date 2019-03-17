b75f54b9c733269cc93b4be61f4589b1b8444b4d
Vunb <nhubaovu@gmail.com>
2019-03-17 07:47:05 +0700

    add lab typescript oriented development

---
+++ lab 02/03. Typescript/ts-express/app/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+// initialize configuration
+const port = process.env.SERVER_PORT || 3000;
+const app = express();
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) =>{
+ // render the index template
+ res.render( "index" );
+} );
+// start the express server
+app.listen( port, () => console.log(`Server started at http://localhost:${ port }`));
