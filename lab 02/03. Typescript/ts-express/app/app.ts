2d1c9dc542d3e727f3c00fa598730279e38b1e3c
Vunb <nhubaovu@gmail.com>
2019-03-18 10:05:44 +0700

    config static view

--- lab 02/03. Typescript/ts-express/app/app.ts
+++ lab 02/03. Typescript/ts-express/app/app.ts
@@ -5,13 +5,17 @@ const port = process.env.SERVER_PORT || 3000;
 const app = express();
 
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Middlewares
+// app.use(express.static('public'));
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) =>{
- // render the index template
- res.render( "index" );
-} );
+app.get("/", (req, res) => {
+  // render the index template
+  res.render("index");
+});
 // start the express server
-app.listen( port, () => console.log(`Server started at http://localhost:${ port }`));
+app.listen(port, () => console.log(`Server started at http://localhost:${ port }`));
+
